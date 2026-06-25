b3e4c28a7d625e851c8a3e192a2509e0cda992df
Preetham <Preetham.k@tezo.com>
2026-06-25 15:24:48 +0530

    Made change in dialog

--- tests/dialog.spec.js
+++ tests/dialog.spec.js
@@ -19,7 +19,7 @@ test('dialogs with prompt',async({page})=>{
     await page.goto('https://demo.automationtesting.in/Alerts.html')
     await page.getByText('Alert with Textbox ').click()
     page.on('dialog',async(dialog)=>{
-        await dialog.accept('hi im preetham')
+        await dialog.accept('hi im preetham sai raj')
     })
     page.click('[onclick="promptbox()"]')
     await expect(page.locator('[id="demo1"]')).toContainText('hi im preetham How are you today')
